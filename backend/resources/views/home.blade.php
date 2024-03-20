aac84408d85216995338a6b07b60e20d1fc24779
MotoPath <33699113+MotoPathG@users.noreply.github.com>
2024-03-21 01:59:54 +0300

    Add frontend

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,23 +1,164 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<div class="promotion">
+        <div class="owl-carousel promotion-slider" id="slider">
+            <div class="item container">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <p class="fs-4 promotion-name">
+                            Hi-Cappa</br>
+                            <span class="fs-3 fw-bold">5.1 - block black</span>
+                        </p>
+                        <p class="fs-6">
+                            Со скидкой <span class="promotion-count">30%</span>
+                            в этом месяце
+                        </p>
+                        <button class="promotion-button">Купить</button>
+                    </div>
+                    <div class="col-6">
+                        <img src="images/hi-cappa51.png" alt="" width="300px">
+                    </div>
+                </div>
+            </div>
+            <div class="item container">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <p class="fs-4 promotion-item__name">
+                            Hi-Cappa</br>
+                            <span class="fs-3 fw-bold">4.1 - block black</span>
+                        </p>
+                        <p class="fs-6">
+                            Со скидкой <span class="promotion-count">45%</span>
+                            в этом месяце
+                        </p>
+                        <button class="promotion-button">Купить</button>
+                    </div>
+                    <div class="col-6">
+                        <img src="images/hi-cappa51.png" alt="" width="300px">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="top-category">
+        <div class="container p-3">
+            <div class="row align-items-center">
+                <div class="col-lg-3 col-12">
+                    <div class="top-category__detals">
+                        <p class="fs-3 fw-bold">Лучше категории в этом месяце</p>
+                        <p class="fs-6">По оценкам пользователей</p>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-12">
+                    <div class="owl-carousel top-slider" id="top-slider">
+                        <div class="item">
+                            <div class="top-category_item">
+                                <img src="images/hi-cappa51.png" />
+                                <p class="m-0">Пистолеты</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="top-category_item">
+                                <img src="images/icon.png" />
+                                <p class="m-0">Одежда</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="top-category_item">
+                                <img src="images/icon.png" />
+                                <p class="m-0">Расходники</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="top-category_item">
+                                <img src="images/icon.png" />
+                                <p class="m-0">Комуникации</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="top-category_item">
+                                <img src="images/icon.png" />
+                                <p class="m-0">Защита</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="new-items">
+        <div class="container py-5">
+            <div class="col-12">
+                <p class="fs-3">Новинки</p>
+                <hr class="hr hr-blurry" />
+            </div>
+            <div class="col-12 py-3 new-items__conteiner">
+                    <div class="col-lg-7 col-12 new-items__box py-5 px-2">
+                        <div class="col-lg-6 col-12 new-items__img_parent">
+                            <img class="new-items__img_image" src="images/hi-cappa51.png" alt="">
+                            <div class="new-items__img_logonew"><span>New</span></div>
+                        </div>
+                        <div class="col-lg-6 col-12 new-items__detals">
+                            <div><span class="price-old fw-bold">$20</span><span class="price-now fw-bold">$15</span></div>
+                            <span>Пистолет Hi-Cappa</span>
+                            <span>
+                                <i class="fa fa-star-o item-good"></i>
+                                <i class="fa fa-star-o item-good"></i>
+                                <i class="fa fa-star-o item-good"></i>
+                                <i class="fa fa-star-o item-good"></i>
+                                <i class="fa fa-star-o item-nogood"></i>
+                            </span>
+                            <p class="text-secondary p-y3">Страйкбольная версия пистолета Hi-Capa 5.1 от фирмы Tokyo Marui, Япония.</br>Принцип действия пистолета: GBB.</br>Корпус пистолета выполнен из ударопрочного ABS-пластика, органы управления металлические. Пистолет работает на Green Gas, который заправляется в магазин. Количество выстрелов зависит от температуры и темпа стрельбы, но в среднем это 2 полных магазина.</p>
+                            <button class="btn">Добавить в корзину</button>
+                        </div>
+                    </div>
+
+
+                    <div class="new-items__gap20">
+                        <div class="col-12 new-items__box py-5 px-2">
+                            <div class="col-lg-6 col-12 new-items__img_parent">
+                                <img class="new-items__img_image" src="images/hi-cappa51.png" alt="">
+                                <div class="new-items__img_logonew"><span>New</span></div>
+                            </div>
+                            <div class="col-lg-6 col-12 new-items__detals">
+                                <div><span class="price-now fw-bold">$15</span></div>
+                                <span>Пистолет Hi-Cappa</span>
+                                <span>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-nogood"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col-12 new-items__box py-5 px-2">
+                            <div class="col-lg-6 col-12 new-items__img_parent">
+                                <img class="new-items__img_image" src="images/hi-cappa51.png" alt="">
+                                <div class="new-items__img_logonew"><span>New</span></div>
+                            </div>
+                            <div class="col-lg-6 col-12 new-items__detals">
+                                <div><span class="price-old fw-bold">$20</span><span class="price-now fw-bold">$15</span></div>
+                                <span>Пистолет Hi-Cappa</span>
+                                <span>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-good"></i>
+                                    <i class="fa fa-star-o item-nogood"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
 </div>
 @endsection
--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -8,53 +8,247 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+          rel="stylesheet">
 
-    <!-- Scripts -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="{{ asset('styles/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('styles/mobile.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('plugins/owlcarousel/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/owlcarousel/assets/owl.theme.default.min.css') }}">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="container">
+        <div class="header">
+            <div class="col-3">
+                <img class="float-start" src="{{ asset('images/icon.png') }}" alt="AIRSOFT" width="100px" />
+            </div>
+            <form class="col-lg-6 col-12 d-flex search-form" action="index.html" method="get">
+                <div class="col-10 d-flex brd">
+                    <div class="col p-0">
+                        <select class="search-type" name="search_type">
+                            <option value="0" selected>Все категории</option>
+                            <option value="1">Основное</option>
+                            <option value="2">Расходники</option>
+                            <option value="3">Одежда</option>
+                            <option value="4">....</option>
+                        </select>
+                    </div>
+                    <div class="col-8 p-0">
+                        <input type="text" name="search_input" class="search-input" required
+                               placeholder="Введите, что вы хотите найти" />
+                    </div>
+                </div>
+                <div class="col p-0">
+                    <button type="submit" class="search-button">Найти</button>
+                </div>
+            </form>
+            <div class="col-3">
+                <div class="header-icons float-end">
+                    <a class="shop-basket" href="#">
+                        <i class="fa fa-shopping-basket shop-basket_icon"></i>
+                        <div class="shop-basket__count"><span>3</span></div>
+                    </a>
+                    @guest
+                    <!--Auth-->
+                        @if (Route::has('login'))
+                            <a href="{{ route('login') }}"><i class="fa fa-user"></i></a>
+                        @endif
+                    @else
+                    <!--Profile IN DEV --UPDATE -->
+                        @if (Route::has('home'))
+                            <a href="{{ route('home') }}"><i class="fa fa-user"></i></a>
+                        @endif
+                    @endguest
+                    <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <i class="fa fa-bars for-phone" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+        <div class="container-fluid">
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Закрыть"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+						    <h5>Меню</h5>
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Главная</a>
+							<hr class="hr hr-blurry m-0" />
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Категории
+                            </a>
+							<hr class="hr hr-blurry m-0" />
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Основное</a></li>
+                                <li><a class="dropdown-item" href="#">Расходники</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Одежда</a></li>
+                            </ul>
+                        </li>
 
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Новинки</a>
+							<hr class="hr hr-blurry m-0" />
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Контакты</a>
+							<hr class="hr hr-blurry m-0" />
+                        </li>
+						<li class="nav-item my-4 phone-menu__icon">
+                            
+                            <a class="circle-link" href="#"><i class="fa fa-shopping-basket"></i></a>
+                            <a class="circle-link" href="#"><i class="fa fa-user"></i></a>
+                        </li>
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="category-pc">
+        <div class="container p-1">
+            <div class="row align-items-center">
+                <ul class="d-flex m-0" style="list-style-type:none;">
+                    <li>
+                        <a href="#" class="btn text-white">
+                            Главная
+                        </a>
+                    </li>
+                    <li>
+                        <div class="btn-group">
+                            <button type="button" class="btn text-white dropdown-toggle" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                Категории
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Основное</a></li>
+                                <li><a class="dropdown-item" href="#">Расходники</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
                                 </li>
-                            @endif
+                                <li><a class="dropdown-item" href="#">Одежда</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="*" class="btn text-white">
+                            Новинки
+                        </a>
+                    </li>
+                    <li>
+                        <a href="*" class="btn text-white">
+                            Контакты
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <main class="py-4">
+            @yield('content')
+    </main>
+    
+    
+    <div class="bottom py-5 container">
+        <div class="bottom-item">
+            <img class="float-start" src="images/icon.png" alt="AIRSOFT" width="100px" />
+            <span>Магазин страйбольной тематики. Найдем все что интересует!</span>
+            <div>
+                <span class="bottom-feedback">Адрес:</span>
+                <span>Росcийская Федерация, Ростовская Область,</br>г. Шахты, ул. Советская, д. 1.</span>
+            </div>
+            <div>
+                <span class="bottom-feedback">Для связи:</span>
+                <a href="tel:+79281945522">8 (928) 194 55 22</a>
+            </div> 
+            <div>
+                <span class="bottom-feedback">Для связи:</span>
+                <a  href="mailto:+support@airsoft.ru&body=AirSoft_Site">support@airsoft.ru</a>
+            </div>
+        </div>
+        <div class="bottom-item">
+            <span class="fw-bold">Продукты</span>
+            <a href="#">Показать цены</a>
+            <a href="#">Новые продукты</a>
+            <a href="#">Лучше цены</a>
+            <a href="#">Свзяться</a>
+            <a href="#">Карта сайта</a>
+            <a href="#">Авторизация</a>
+        </div>
+        <div class="bottom-item">
+            <span class="fw-bold">О компании</span>
+            <a href="#">Доставка</a>
+            <a href="#">Документы</a>
+            <a href="#">О нас</a>
+            <a href="#">Безопасность оплаты</a>
+            <a href="#" target="_blank">Карта сайта</a>
+            <a href="#">Магазин</a>
+        </div>
+        <div class="bottom-item  bottom-space">
+            <div class="bottom-item">
+                <span class="fw-bold">Подписаться</span>
+                <span class="sosial-block">
+                    <a class="social-icons" href="#"><i class="fa fa-vk"></i></a>
+                    <a class="social-icons" href="#"><i class="fa fa-instagram"></i></a>
+                    <a class="social-icons" href="#"><i class="fa fa-youtube"></i></a>
+                    <a class="social-icons" href="#"><i class="fa fa-google-plus"></i></a>
+                    <a class="social-icons" href="#"><i class="fa fa-instagram"></i></a>
+                </span>
+            </div>
+            <div class="bottom-item">
+                <span class="fw-bold">Получать новости</span>
+                <form class="search-form" action="#" method="post">
+                   <div class="d-flex">
+                        <div class="col-8 p-0">
+                           <input type="email" name="email_get_news" class="email_get_news" required
+                                  placeholder="Введите ваш email" />
+                        </div>
+                        <div class="col p-0">
+                            <button type="submit" class="search-button">Отправить</button>
+                        </div>
+                    </div>
+               </form>
+            </div>
+        </div>
+    </div>
+    <div class="coppyryte-box py-4">
+        <span>Coppirate <a href="#MPZ">MPZ</a>. All Rights Reserved</span>
+    </div>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+    
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+<!-- Authentication Links -->
+@guest
+@if (Route::has('login'))
+<li class="nav-item">
+<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+</li>
+@endif
+
+@if (Route::has('register'))
+<li class="nav-item">
+<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+</li>
+@endif
+@else
+{{ Auth::user()->name }}
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -66,17 +260,17 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+@endguest
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <script src="plugins/jquery-3.7.1.min.js"></script>
+    <script src="plugins/owlcarousel/owl.carousel.min.js"></script>
+    <script src="js/index.js"></script>
 </body>
 </html>
